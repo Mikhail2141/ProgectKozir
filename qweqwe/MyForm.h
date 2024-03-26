aa93ee295970b4edc51f8b6a3e938cdff95b11ad
Student <Student@USER-PC>
2024-03-26 19:30:57 +0300

    кнопка

--- qweqwe/MyForm.h
+++ qweqwe/MyForm.h
@@ -39,6 +39,7 @@ namespace qweqwe {
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::TextBox^ textBox2;
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button2;
 	protected:
 
 	private:
@@ -58,13 +59,14 @@ namespace qweqwe {
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(130, 193);
+			this->button1->Location = System::Drawing::Point(111, 166);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(146, 42);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = true;
@@ -92,11 +94,21 @@ namespace qweqwe {
 			this->label1->Size = System::Drawing::Size(0, 13);
 			this->label1->TabIndex = 3;
 			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(111, 238);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(146, 42);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(385, 340);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
