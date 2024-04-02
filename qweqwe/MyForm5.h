9cbb4e96c62c01830c8178f3c5ff2eade7ca376c
Student <Student@USER-PC>
2024-04-02 19:42:18 +0300

    Добавлен раздел Геомтрия

--- qweqwe/MyForm5.h
+++ qweqwe/MyForm5.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "MyForm.h"
+#include "MyForm6.h"
 namespace qweqwe {
 
 	using namespace System;
@@ -16,6 +17,9 @@ namespace qweqwe {
 	{
 	public:
 		MyForm^ F;
+	private: System::Windows::Forms::Button^ button2;
+	public:
+		MyForm6^ F6;
 		MyForm5(void)
 		{
 			InitializeComponent();
@@ -37,7 +41,7 @@ namespace qweqwe {
 		}
 	private: System::Windows::Forms::Button^ button1;
 	protected:
-	private: System::Windows::Forms::Button^ button2;
+
 	private: System::Windows::Forms::Label^ label7;
 
 	private:
@@ -54,53 +58,50 @@ namespace qweqwe {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold));
-			this->button1->Location = System::Drawing::Point(189, 105);
-			this->button1->Margin = System::Windows::Forms::Padding(4);
+			this->button1->Location = System::Drawing::Point(142, 85);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(228, 52);
+			this->button1->Size = System::Drawing::Size(171, 42);
 			this->button1->TabIndex = 1;
 			this->button1->Text = L"Алгебра";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm5::button1_Click);
 			// 
-			// button2
-			// 
-			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold));
-			this->button2->Location = System::Drawing::Point(189, 211);
-			this->button2->Margin = System::Windows::Forms::Padding(4);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(228, 52);
-			this->button2->TabIndex = 2;
-			this->button2->Text = L"Геометрия";
-			this->button2->UseVisualStyleBackColor = true;
-			// 
 			// label7
 			// 
 			this->label7->AutoSize = true;
 			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold));
-			this->label7->Location = System::Drawing::Point(133, 24);
-			this->label7->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label7->Location = System::Drawing::Point(100, 20);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(345, 29);
+			this->label7->Size = System::Drawing::Size(273, 24);
 			this->label7->TabIndex = 11;
 			this->label7->Text = L"Решебник задач \"Козырь\" ";
 			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold));
+			this->button2->Location = System::Drawing::Point(142, 153);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(171, 42);
+			this->button2->TabIndex = 12;
+			this->button2->Text = L"Геометрия";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm5::button2_Click);
+			// 
 			// MyForm5
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(613, 402);
-			this->Controls->Add(this->label7);
+			this->ClientSize = System::Drawing::Size(460, 327);
 			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label7);
 			this->Controls->Add(this->button1);
-			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"MyForm5";
 			this->Text = L"Munu";
 			this->ResumeLayout(false);
@@ -114,6 +115,13 @@ namespace qweqwe {
 		F->Show();
 
 	}
+	
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	 
+		F6 = gcnew MyForm6();
+		F6->Show();
+	}
+	
 	
 };
 }
