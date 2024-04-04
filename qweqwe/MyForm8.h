26b2122851f2858409ec185cec7a19057ac36b20
Student <Student@USER-PC>
2024-04-04 19:20:50 +0300

    Доделал форму треугольника

--- qweqwe/MyForm8.h
+++ qweqwe/MyForm8.h
@@ -40,6 +40,7 @@ namespace qweqwe {
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::Label^ label5;
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label3;
 
 
 	protected:
@@ -65,6 +66,7 @@ namespace qweqwe {
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -131,14 +133,27 @@ namespace qweqwe {
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(103, 52);
 			this->button1->TabIndex = 9;
-			this->button1->Text = L"с";
+			this->button1->Text = L"Решить";
 			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(23, 75);
+			this->label3->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(167, 20);
+			this->label3->TabIndex = 10;
+			this->label3->Text = L"Сторона квадрата";
 			// 
 			// MyForm8
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(384, 261);
+			this->Controls->Add(this->label3);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
