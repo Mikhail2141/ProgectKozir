c545f41b6cdfb4d369095e1cf02443f6d4fdd167
jak <jak@DESKTOP-JTACVK3>
2024-04-10 20:48:38 +0300

    Добавлены формы для раздела Динамика

--- qweqwe/MyForm14.h
+++ qweqwe/MyForm14.h
@@ -1,4 +1,7 @@
 #pragma once
+#include "MyForm19.h"
+#include "MyForm20.h"
+#include "MyForm21.h"
 
 namespace qweqwe {
 
@@ -15,6 +18,11 @@ namespace qweqwe {
 	public ref class MyForm14 : public System::Windows::Forms::Form
 	{
 	public:
+
+		MyForm19^ F19;
+		MyForm20^ F20;
+		MyForm21^ F21;
+
 		MyForm14(void)
 		{
 			InitializeComponent();
@@ -60,46 +68,67 @@ namespace qweqwe {
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold));
-			this->button1->Location = System::Drawing::Point(68, 77);
+			this->button1->Location = System::Drawing::Point(91, 95);
+			this->button1->Margin = System::Windows::Forms::Padding(4);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(171, 42);
+			this->button1->Size = System::Drawing::Size(228, 52);
 			this->button1->TabIndex = 2;
 			this->button1->Text = L"Давление";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm14::button1_Click);
 			// 
 			// button2
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold));
-			this->button2->Location = System::Drawing::Point(68, 146);
+			this->button2->Location = System::Drawing::Point(91, 180);
+			this->button2->Margin = System::Windows::Forms::Padding(4);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(171, 42);
+			this->button2->Size = System::Drawing::Size(228, 52);
 			this->button2->TabIndex = 3;
 			this->button2->Text = L"Сила тяжести";
 			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm14::button2_Click);
 			// 
 			// button3
 			// 
 			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold));
-			this->button3->Location = System::Drawing::Point(43, 12);
+			this->button3->Location = System::Drawing::Point(57, 15);
+			this->button3->Margin = System::Windows::Forms::Padding(4);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(229, 42);
+			this->button3->Size = System::Drawing::Size(305, 52);
 			this->button3->TabIndex = 4;
 			this->button3->Text = L"Плотность вещества";
 			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm14::button3_Click);
 			// 
 			// MyForm14
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(314, 261);
+			this->ClientSize = System::Drawing::Size(419, 321);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"MyForm14";
 			this->Text = L"MyForm14";
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	};
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		F19 = gcnew MyForm19();
+		F19->Show();
+
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		F20 = gcnew MyForm20();
+		F20->Show();
+
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		F21 = gcnew MyForm21();
+		F21->Show();
+	}
+};
 }
