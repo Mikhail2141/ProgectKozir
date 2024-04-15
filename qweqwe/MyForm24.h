04f8596850f409906cd78ac2f27cf4d33be51313
waby <waby@DESKTOP-P21L3TV>
2024-04-15 22:04:04 +0300

    выод в разделе геометрия-паралеллограмм.

--- qweqwe/MyForm24.h
+++ qweqwe/MyForm24.h
@@ -79,9 +79,10 @@ namespace qweqwe {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(90, 9);
+			this->label1->Location = System::Drawing::Point(68, 7);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(325, 25);
+			this->label1->Size = System::Drawing::Size(267, 20);
 			this->label1->TabIndex = 8;
 			this->label1->Text = L"Расстояние, скорость, время  ";
 			// 
@@ -90,9 +91,10 @@ namespace qweqwe {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(12, 56);
+			this->label2->Location = System::Drawing::Point(9, 46);
+			this->label2->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(171, 25);
+			this->label2->Size = System::Drawing::Size(140, 20);
 			this->label2->TabIndex = 9;
 			this->label2->Text = L"S - расстояние ";
 			// 
@@ -101,9 +103,10 @@ namespace qweqwe {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label3->Location = System::Drawing::Point(12, 142);
+			this->label3->Location = System::Drawing::Point(9, 115);
+			this->label3->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(121, 25);
+			this->label3->Size = System::Drawing::Size(98, 20);
 			this->label3->TabIndex = 10;
 			this->label3->Text = L"t -  время  ";
 			// 
@@ -112,49 +115,45 @@ namespace qweqwe {
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label4->Location = System::Drawing::Point(12, 99);
+			this->label4->Location = System::Drawing::Point(9, 80);
+			this->label4->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(146, 25);
+			this->label4->Size = System::Drawing::Size(119, 20);
 			this->label4->TabIndex = 11;
 			this->label4->Text = L"V - скорость ";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(232, 142);
-			this->textBox1->Margin = System::Windows::Forms::Padding(4);
+			this->textBox1->Location = System::Drawing::Point(174, 115);
 			this->textBox1->Multiline = true;
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(49, 35);
+			this->textBox1->Size = System::Drawing::Size(38, 29);
 			this->textBox1->TabIndex = 24;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(232, 99);
-			this->textBox2->Margin = System::Windows::Forms::Padding(4);
+			this->textBox2->Location = System::Drawing::Point(174, 80);
 			this->textBox2->Multiline = true;
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(49, 35);
+			this->textBox2->Size = System::Drawing::Size(38, 29);
 			this->textBox2->TabIndex = 25;
-			 
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(232, 56);
-			this->textBox3->Margin = System::Windows::Forms::Padding(4);
+			this->textBox3->Location = System::Drawing::Point(174, 46);
 			this->textBox3->Multiline = true;
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(49, 35);
+			this->textBox3->Size = System::Drawing::Size(38, 29);
 			this->textBox3->TabIndex = 26;
-			 
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(183, 345);
-			this->button1->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->button1->Location = System::Drawing::Point(137, 280);
+			this->button1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(137, 64);
+			this->button1->Size = System::Drawing::Size(103, 52);
 			this->button1->TabIndex = 27;
 			this->button1->Text = L"Решить";
 			this->button1->UseVisualStyleBackColor = true;
@@ -165,9 +164,10 @@ namespace qweqwe {
 			this->label5->AutoSize = true;
 			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label5->Location = System::Drawing::Point(12, 298);
+			this->label5->Location = System::Drawing::Point(9, 242);
+			this->label5->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(76, 25);
+			this->label5->Size = System::Drawing::Size(60, 20);
 			this->label5->TabIndex = 28;
 			this->label5->Text = L"t = S/v";
 			// 
@@ -176,9 +176,10 @@ namespace qweqwe {
 			this->label6->AutoSize = true;
 			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label6->Location = System::Drawing::Point(12, 250);
+			this->label6->Location = System::Drawing::Point(9, 203);
+			this->label6->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(76, 25);
+			this->label6->Size = System::Drawing::Size(60, 20);
 			this->label6->TabIndex = 29;
 			this->label6->Text = L"v = S/t";
 			// 
@@ -187,17 +188,18 @@ namespace qweqwe {
 			this->label7->AutoSize = true;
 			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label7->Location = System::Drawing::Point(12, 212);
+			this->label7->Location = System::Drawing::Point(9, 172);
+			this->label7->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(69, 25);
+			this->label7->Size = System::Drawing::Size(55, 20);
 			this->label7->TabIndex = 30;
 			this->label7->Text = L"S = vt";
 			// 
 			// MyForm24
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(459, 424);
+			this->ClientSize = System::Drawing::Size(344, 344);
 			this->Controls->Add(this->label7);
 			this->Controls->Add(this->label6);
 			this->Controls->Add(this->label5);
@@ -209,8 +211,10 @@ namespace qweqwe {
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"MyForm24";
 			this->Text = L"MyForm24";
+			this->Load += gcnew System::EventHandler(this, &MyForm24::MyForm24_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -248,5 +252,7 @@ namespace qweqwe {
 
 	}
  
+private: System::Void MyForm24_Load(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
